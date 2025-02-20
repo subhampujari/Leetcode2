0071f0998990a8f5ba390bd8a6a98012c1224c1b
Subham <subhampujari541@gmail.com.>
2025-02-20 09:39:56 -0800

    StringBinary

---
+++ UniqueBinaryString.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueBinaryString{
+    public String findDifferentBinaryString(String[] nums) {
+        Set<Integer> integers = new HashSet();
+        for (String num : nums) {
+            integers.add(Integer.parseInt(num, 2));
+        }
+        
+        int n = nums.length;
+        for (int num = 0; num <= n; num++) {
+            if (!integers.contains(num)) {
+                String ans = Integer.toBinaryString(num);
+                while (ans.length() < n) {
+                    ans = "0" + ans;
+                }
+                
+                return ans;
+            }
+        }
+        
+        return "";
+    }
+}
