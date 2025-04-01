5bced2f5fd58d15eeab9a56cbd599c3695a268ce
Subham <subhampujari541@gmail.com.>
2025-04-01 06:07:18 -0700

    BrainPower

---
+++ BrainPower.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class BrainPower{
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n+1];
+        Arrays.fill(dp, 0);
+        for(int i=n-1; i>=0 ;i--){
+            int point = questions[i][0];
+            int jump = questions[i][1];
+
+            int nextQuestion = Math.min(n, i+jump+1);
+            dp[i] = Math.max(dp[i+1], point + dp[nextQuestion]);
+        }
+        return dp[0];
+    }
+}
